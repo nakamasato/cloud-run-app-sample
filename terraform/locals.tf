0a42aea88b160dc1e16b63d26e215544b2249812
Naka Masato <masatonaka1989@gmail.com>
2024-06-30 08:14:05 +0900

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -18,7 +18,7 @@ locals {
     "roles/iam.workloadIdentityPoolViewer",  # To allow GitHub Actions to use Workload Identity `iam.workloadIdentityPools.get`
     # "roles/iam.workloadIdentityUser",        # To allow GitHub Actions to use Workload Identity
     "roles/iam.serviceAccountAdmin", # To manage other service account
-    # "roles/iam.serviceAccountUser",          # To allow github-actions sa to deploy cloud run (with specified sa)
+    "roles/iam.serviceAccountUser",          # To allow github-actions sa to deploy cloud run (with specified sa) (`iam.serviceAccounts.actAs`)
     "roles/resourcemanager.projectIamAdmin", # GitHub Actions identity
     "roles/run.developer",                   # To allow GitHub Actions to deploy to Cloud Run
     # "roles/run.admin",                       # to grant run.invoker (Permission 'run.services.setIamPolicy' is necessary)
